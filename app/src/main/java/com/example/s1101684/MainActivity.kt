96e10e4100e39363fc8fa23ce6c1694ddfddb9a7
Wrrrrryyyyyy <s1101684@gm.pu.edu.tw>
2023-05-31 11:46:02 +0800

    v.3

--- app/src/main/java/com/example/s1101684/MainActivity.kt
+++ app/src/main/java/com/example/s1101684/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.s1101684
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
@@ -11,6 +13,9 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.s1101684.ui.theme.S1101684Theme
@@ -31,13 +36,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Column {
-        Text(text = "$name"
-        )
-        Image(
-            painter = painterResource(id = R.drawable.map),
-            contentDescription = "地圖"
-        )
+    Box(modifier = Modifier.fillMaxSize()) {
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            drawRect(
+                color = Color.Blue,
+                topLeft = Offset(size.width - 400f, size.height - 200f),
+                size = Size(400f, 200f)
+            )
+            drawRect(
+                color = Color.Blue,
+                topLeft = Offset(0f, 0f),
+                size = Size(400f, 200f)
+            )
+        }
+        Column {
+            Text(text = "$name")
+            Image(
+                painter = painterResource(id = R.drawable.map),
+                contentDescription = "地圖"
+            )
+        }
     }
 }
 
