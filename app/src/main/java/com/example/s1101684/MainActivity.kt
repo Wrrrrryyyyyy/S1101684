c64b778010f27e0141e7a2396a745060f9b3b4e1
Wrrrrryyyyyy <s1101684@gm.pu.edu.tw>
2023-05-31 11:24:33 +0800

    v.2

--- app/src/main/java/com/example/s1101684/MainActivity.kt
+++ app/src/main/java/com/example/s1101684/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.s1101684
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.s1101684.ui.theme.S1101684Theme
 
@@ -19,7 +22,7 @@ class MainActivity : ComponentActivity() {
             S1101684Theme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Greeting("作者：資管2A 郭家瑋")
                 }
             }
         }
@@ -28,13 +31,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Column {
+        Text(text = "$name"
+        )
+        Image(
+            painter = painterResource(id = R.drawable.map),
+            contentDescription = "地圖"
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     S1101684Theme {
-        Greeting("Android")
+        Greeting("作者：資管2A 郭家瑋")
     }
 }
